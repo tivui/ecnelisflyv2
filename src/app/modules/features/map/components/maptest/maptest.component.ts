9fa7db7d9014240f983dc690502ff13e9c71b775
thierry.vuillermet <thierry.vuillermet@cva-engineering.com>
2024-10-16 15:57:03 +0200

    rafraîchissement des cartes côté sidebar

--- src/app/modules/features/map/components/maptest/maptest.component.ts
+++ src/app/modules/features/map/components/maptest/maptest.component.ts
@@ -1,29 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import L, { latLng, tileLayer, polygon, Map, LatLngBounds } from 'leaflet';
 import { ProjectService } from '../../../../core/services/project.service';
 
 @Component({
   selector: 'app-maptest',
   templateUrl: './maptest.component.html',
-  styleUrls: ['./maptest.component.scss'] // Remarque : styleUrls et non styleUrl
+  styleUrls: ['./maptest.component.scss']
 })
 export class MaptestComponent {
   public options = {
     layers: [
-      L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", { maxZoom: 18, minZoom: 9 })
+      L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", { maxZoom: 18, minZoom: 1 })
     ],
-    zoom: 9,
-    center: L.latLng(-4.698904320291842, 11.637450551548591), // Exemple de coordonnées
+    zoom: 15,
+    center: L.latLng(48.8566, 2.29), // Paris comme centre par défaut
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   };
 
   public map!: Map;
   polygons: any[] = [];
-  projects: any[] = [];
-  filteredProjects: any[] = [];
+  projects: any[] = [];       // Tous les projets disponibles dans la zone
+  filteredProjects: any[] = [];  // Projets filtrés, affichés sur la carte et dans la sidenav
 
-  constructor(private projectService: ProjectService) { }
+  constructor(private projectService: ProjectService, private cdr: ChangeDetectorRef) { }
 
+  // Méthode appelée quand la carte est prête
   onMapReady(map: Map) {
     this.map = map;
     this.loadProjects(map.getBounds());
@@ -33,19 +34,24 @@ export class MaptestComponent {
     this.map.on('moveend', () => this.onMoveEnd());
   }
 
+  // Méthode appelée à la fin du zoom
   onZoomEnd() {
-    this.loadProjects(this.map.getBounds());
+    this.loadProjects(this.map.getBounds()); // Recharge les projets dans la zone visible
   }
 
+  // Méthode appelée à la fin du déplacement de la carte
   onMoveEnd() {
-    this.loadProjects(this.map.getBounds());
+    this.loadProjects(this.map.getBounds()); // Recharge les projets dans la zone visible
   }
 
+  // Charge les projets qui sont dans les bounds visibles sur la carte
   loadProjects(bounds: LatLngBounds) {
     const projects = this.projectService.getProjectsInBounds(bounds);
 
+    // Nettoyage des anciens polygones sur la carte
     this.clearPolygons();
 
+    // Ajout des nouveaux polygones visibles sur la carte
     projects.forEach(project => {
       if (project.coordinates.every(coord => coord.length === 2)) {
         const poly = polygon(project.coordinates as [number, number][]).addTo(this.map);
@@ -55,19 +61,43 @@ export class MaptestComponent {
       }
     });
 
-    // Mettre à jour les projets filtrés (par exemple dans la sidenav)
-    this.filteredProjects = projects;
+    // Mettre à jour les projets visibles dans la sidenav
+    this.filteredProjects = this.filterProjectsInBounds(projects, bounds);
+
+    // Forcer la détection de changement après avoir mis à jour filteredProjects
+    this.cdr.detectChanges();
   }
 
+  // Filtre les projets qui sont dans les bounds visibles de la carte
+  filterProjectsInBounds(projects: any[], bounds: LatLngBounds): any[] {
+    return projects.filter(project =>
+      this.isProjectInBounds(project, bounds)
+    );
+  }
+
+  // Vérifie si un projet se trouve dans les bounds visibles de la carte
+  isProjectInBounds(project: any, bounds: LatLngBounds): boolean {
+    // On vérifie si au moins un point du projet est dans les bounds visibles
+    return project.coordinates.some((coord: [number, number]) => {
+      const latLngPoint = latLng(coord[0], coord[1]);
+      return bounds.contains(latLngPoint);
+    });
+  }
+
+  // Méthode pour supprimer tous les polygones précédents sur la carte
   clearPolygons() {
     this.polygons.forEach(polygon => this.map.removeLayer(polygon));
     this.polygons = [];
   }
 
+  // Méthode appelée lors de la sélection d'un projet dans la sidenav
   selectProject(project: any) {
-    // Logique pour centrer la carte sur un projet spécifique lors de la sélection dans la sidenav
+    // Logique pour centrer la carte sur un projet sélectionné dans la sidenav
     if (project.coordinates && project.coordinates.length > 0) {
       this.map.fitBounds(project.coordinates as [number, number][]);
+    } else {
+      // Centrer sur Paris si le projet n'a pas de coordonnées valides
+      this.map.setView([48.8566, 2.3522], this.map.getZoom());
     }
   }
 }
